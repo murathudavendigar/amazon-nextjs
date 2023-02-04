08d5c100227e93f53c9515f4084b654915fde3d7
Captain Price <murathoncu@gmail.com>
2023-02-04 17:22:06 +0300

    add next auth

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -8,12 +8,17 @@ const Checkout = () => {
 
       <main className="lg:flex max-w-screen-2xl mx-auto">
         {/* Left */}
-        <div>
+        <div className="flex-grow m-5 shadow-sm">
           <Image
             src="https://www.junglescout.com/wp-content/uploads/2020/05/Prime-day-banner.png"
             width={1020}
             height={250}
+            className="object-contain"
+            loading="lazy"
           />
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+          </div>
         </div>
 
         {/* Right */}
